f001548b12831ed8ddc8e9573d396f8c012d34db
Justin <justin.nicola@gmx.net>
2019-11-16 14:24:48 +0100

    Code Improvements Socket connection established via Sender class Improved smiling recognition

--- Python/src/main.py
+++ Python/src/main.py
@@ -1,3 +1,24 @@
+
+class Sender(object):
+    s = None
+    def __init__(self):
+        import socket
+        global s
+        TCP_IP = '10.12.1.189'
+        TCP_PORT = 8585
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((TCP_IP, TCP_PORT))
+
+    def send(self, message):
+        if (s != None):
+            b = bytes(message, 'utf-8')
+            s.send(b)
+            return True
+        else:
+            print("The Socket is None")
+            return False
+
+
 from scipy.spatial import distance as dist
 from imutils.video import VideoStream
 from imutils import face_utils
@@ -6,17 +27,33 @@ import imutils
 import time
 import dlib
 import cv2
-
+import socket
 import os
 
+#used to break from via inner loop the outer loop
+endProgram = False
+#if false: picture will be taken, if true: jump to draw contours and continue // Requires refresh after end
 hasTakenImage = False
+#if false: loop further, if true: countdown waiting time and trigger smileGoalReached
 isSmiling = False
+#if false: loop further, if true: program ends // Requires refresh after end
 smileGoalReached = False
 
-startingTime = -1
-waitingTime = 2000
-destinationTime = -1
+coffeeSent = False
 
+WAIT_TIME_UNTIL_PICTURE = 2000
+WAIT_TIME_AFTER_SMILEGOAL = 2000
+WAIT_TIME_FOR_SENDING = 1000
+DRAW_CONTOURS = True
+
+waitForPictureStartingTime = -1 #Requires refresh after end
+waitForPictureDestinationTime = -1 #Requires refresh after end
+
+waitForNextUserStartingTime = -1 #Requires refresh after end
+waitForNextUserDestinationTime = -1 #Requires refresh after end
+
+waitForNextSendStartingTime = -1 #Requires refresh after end
+waitForNextSendDestinationTime = -1 #Requires refresh after end
 LANDMARKS_FILE = "./face_recognition/examples/shape_predictor_68_face_landmarks.dat"
 
 # check if landmarks file available
@@ -31,13 +68,12 @@ smileGoal = 5
 # # alarm
 EYE_AR_THRESH = 0.3
 EYE_AR_CONSEC_FRAMES = 48
-neutralMouth = 50000
+neutralMouth = -1
 smileDuration = 0
 
 # initialize the frame counter as well as a boolean used to
 # indicate if the alarm is going off
 COUNTER = 0
-doDraw = True
 
 
 def input():
@@ -68,12 +104,11 @@ def manageStream(b):
 
 def takePic(frame):
     global hasTakenImage
-    global doDraw
+    global DRAW_CONTOURS
     cv2.imwrite("open_cv_frame.png", frame)
     print("{} written!".format("open_cv_frame.png"))
     hasTakenImage = True
-    doDraw = True
-    analyzePic("open_cv_frame.png")
+    DRAW_CONTOURS = True
 
 def analyzePic (pathToPic):
     detector = dlib.get_frontal_face_detector()
@@ -106,21 +141,18 @@ def eyeAspectRatio(eye):
     # return the eye aspect ratio
     return ear
 
-
 def smileDetector(face, ear):
     global isSmiling
     currentMouthRatio = dist.euclidean(face[49], face[55])
-
+    print(neutralMouth - currentMouthRatio)
     if neutralMouth is not 50000:
         mr = neutralMouth - currentMouthRatio
         # if ear <= 0.25 and mr < -5:
-        if ear <= 0.4 and mr < -1:
-
+        if ear <= 0.3 and mr < 5:
             isSmiling = True
-
         else:
             isSmiling = False
-
+            print("Not smiling!")
 
 def drawContours(shape, frame):
     (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
@@ -142,7 +174,7 @@ def drawContours(shape, frame):
     leftEyeHull = cv2.convexHull(leftEye)
     rightEyeHull = cv2.convexHull(rightEye)
     mouthHull = cv2.convexHull(mouth)
-    if (doDraw):
+    if (DRAW_CONTOURS):
         cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
         cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
         cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
@@ -158,14 +190,44 @@ def drawContours(shape, frame):
             cv2.putText(frame, "Kaffee unterwegs :)", (50, 30),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
+def resetAllGlobals():
+    global coffeeSent
+    global waitForNextUserStartingTime
+    global waitForNextUserDestinationTime
+    global waitForPictureStartingTime
+    global waitForPictureDestinationTime
+    global hasTakenImage
+    global smileGoalReached
+
+    waitForNextUserDestinationTime = -1
+    waitForNextUserStartingTime = -1
+    waitForPictureDestinationTime = -1
+    waitForPictureStartingTime = -1
+    hasTakenImage = False
+    smileGoalReached = False
+    coffeeSent = False
 
 def detectFacialExpressions(vs):
-    global startingTime
-    global destinationTime
-    global waitingTime
-    global doDraw
+    global DRAW_CONTOURS
+    global WAIT_TIME_AFTER_SMILEGOAL
+    global WAIT_TIME_UNTIL_PICTURE
+    global WAIT_TIME_FOR_SENDING
+
+    #Waiting Times - Must be refreshed after each smile goal
+    global waitForNextUserStartingTime
+    global waitForNextUserDestinationTime
+
+    global waitForPictureStartingTime
+    global waitForPictureDestinationTime
+
+    global waitForNextSendStartingTime
+    global waitForNextSendDestinationTime
+
+    global smileGoalReached
+    global endProgram
 
     print("[INFO] loading facial landmark predictor...")
+    print(smileGoalReached)
     detector = dlib.get_frontal_face_detector()
     # predictor = dlib.shape_predictor(args["shape_predictor"])
     predictor = dlib.shape_predictor(LANDMARKS_FILE)
@@ -177,52 +239,79 @@ def detectFacialExpressions(vs):
 
         rects = detector(gray, 0)
 
-        for rect in rects:
-            shape = predictor(gray, rect)
+        if not (smileGoalReached):
+            for rect in rects:
+                shape = predictor(gray, rect)
 
-            if not hasTakenImage:
-                doDraw = False
-                if (startingTime == -1 and destinationTime == -1):
-                    startingTime = int(round(time.time() * 1000))
-                    destinationTime = waitingTime + startingTime
+                if not hasTakenImage:
+                    DRAW_CONTOURS = False
+                    if (waitForPictureStartingTime == -1 and waitForPictureDestinationTime == -1):
+                        waitForPictureStartingTime = int(round(time.time() * 1000))
+                        waitForPictureDestinationTime = WAIT_TIME_UNTIL_PICTURE + waitForPictureStartingTime
 
-                currentTime = int(round(time.time() * 1000))
-                if (destinationTime - currentTime <= 0 and destinationTime > 0):
-                    takePic(frame)
+                    currentTime = int(round(time.time() * 1000))
+                    if (waitForPictureDestinationTime - currentTime <= 0 and waitForPictureDestinationTime > 0):
+                        takePic(frame)
+                        analyzePic("open_cv_frame.png")
+
+                drawContours(shape, frame)
+                if not smileGoalReached:
+                    if isSmiling:
+                        global smileDuration
+                        global smileStart
+                        global smileGoal
+                        if smileStart == 0:
+                            smileStart = time.time()
+
+                        smileDuration = int(round(time.time() - smileStart))
+                        if smileDuration > 2:
+                            smileGoalReached = True
 
 
-                    # maybe insert a reset for destinationTime and startingTime
+                    elif not isSmiling:
+                        smileStart = 0
+                        smileDuration = 0
+        else:
+            global coffeeSent
+            currentTime = int(round(time.time() * 1000))
 
-            drawContours(shape, frame)
-            global smileGoalReached
-            if not smileGoalReached:
-                if isSmiling:
-                    global smileDuration
-                    global smileStart
-                    global smileGoal
-                    if smileStart == 0:
-                        smileStart = time.time()
+            if waitForNextSendStartingTime == -1 and waitForNextSendDestinationTime == -1:
+                print("Sender")
+                # sender = Sender()
+                # coffeeSent = sender.send("KAFFEE_1")
+                waitForNextSendStartingTime = int(round(time.time() * 1000))
+                waitForNextSendDestinationTime = WAIT_TIME_FOR_SENDING + waitForNextSendStartingTime
+            
+            if (waitForNextSendDestinationTime - currentTime <= 0 < waitForNextSendDestinationTime):
+                waitForNextSendDestinationTime = -1
+                waitForNextSendStartingTime = -1
+                print("Reset")
 
-                    #print(time.time() - smileStart)
-                    smileDuration = int(round(time.time() - smileStart))
-                    if smileDuration > 5:
-                        smileGoalReached = True
+            if waitForNextUserStartingTime == -1 and waitForNextUserDestinationTime == -1:
+                waitForNextUserStartingTime = int(round(time.time() * 1000))
+                waitForNextUserDestinationTime = WAIT_TIME_AFTER_SMILEGOAL + waitForNextUserStartingTime
 
-                elif not isSmiling:
-                    smileStart = 0
-                    smileDuration = 0
+            currentTime = int(round(time.time() * 1000))
+            if waitForPictureDestinationTime - currentTime <= 0 < waitForPictureDestinationTime:
+                resetAllGlobals()
+                break
+
 
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
-        #print(smileEnd-smileStart)
-        # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
+        if key == ord("e"):
+            endProgram = True
             break
+        if key == ord("s"):
+            smileGoalReached = True
 
-
-# args = input()
 vs = manageStream(True)
-detectFacialExpressions(vs)
+
+while True:
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q") or endProgram:
+        break
+    detectFacialExpressions(vs)
 
 
 cv2.destroyAllWindows()
