e6c1a2145a7e30d3d4a3fd4b69bf9f22d08c4ee9
Diego Botache <diego.botache@uni-kassel.de>
2019-11-17 10:45:38 +0100

    new structure, missing some implementations

---
+++ Python/src/run_coffee.py
@@ -0,0 +1,74 @@
+import os
+import argparse
+import imutils
+import cv2
+import dlib
+
+from imutils.video import VideoStream
+from imutils import face_utils
+import time
+
+from utils import smileDetector, eyeAspectRatio
+
+from utils import drawContours
+
+
+from sender import Sender
+
+LANDMARKS_FILE = "./face_recognition/examples/shape_predictor_68_face_landmarks.dat"
+
+# check if landmarks file available
+assert os.path.isfile(LANDMARKS_FILE)
+
+WEBCAM = 0
+
+def input():
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--shape-predictor", default=LANDMARKS_FILE,
+                    help="path to facial landmark predictor")
+    ap.add_argument("-w", "--webcam", type=int, default=0,
+                    help="index of webcam on system")
+    args = vars(ap.parse_args())
+    return args
+
+
+def main():
+    args = input()
+    print("[INFO] loading facial landmark predictor...")
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(args["shape_predictor"])
+
+    vs = VideoStream(WEBCAM).start()
+    time.sleep(1.0)
+
+    while True:
+        # grab the frame from the threaded video file stream, resize
+        # it, and convert it to grayscale
+        # channels)
+        frame = vs.read()
+        frame = imutils.resize(frame, width=450)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # detect faces in the grayscale frame
+        rects = detector(gray, 0)
+
+        # loop over the face detections
+        for rect in rects:
+            shape = predictor(gray, rect)
+            drawContours(shape, frame)
+
+        # show the frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+
+    cv2.destroyAllWindows()
+    vs.stop()
+
+
+if __name__=="__main__":
+    main()
---
+++ Python/src/utils.py
@@ -0,0 +1,105 @@
+import cv2
+import dlib
+import imutils
+from imutils import face_utils
+from scipy.spatial import distance as dist
+
+import numpy as np
+
+
+def takePic(frame):
+    global hasTakenImage
+    global DRAW_CONTOURS
+    cv2.imwrite("open_cv_frame.png", frame)
+    print("{} written!".format("open_cv_frame.png"))
+    hasTakenImage = True
+    DRAW_CONTOURS = True
+
+
+def analyzePic (pathToPic, LANDMARKS_FILE):
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(LANDMARKS_FILE)
+    image = cv2.imread(pathToPic)
+    image = imutils.resize(image, width=700)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    rects = detector(gray, 0)
+    for rect in rects:
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        global neutralMouth
+        neutralMouth = dist.euclidean(shape[49], shape[55])
+
+def eyeAspectRatio(eye):
+    # compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    C = dist.euclidean(eye[0], eye[3])
+
+    # compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+
+    # return the eye aspect ratio
+    return ear
+
+def smileDetector(face, ear):
+
+    print(np.mean(face, 0))
+
+    neutralMouth = 50000
+    isSmiling = False
+    currentMouthRatio = dist.euclidean(face[49], face[55])
+
+
+    print("current mouth ratio: ", currentMouthRatio)
+
+
+    mr = neutralMouth - currentMouthRatio
+    if ear <= 0.5 and mr < -5:
+        isSmiling = True
+        return isSmiling
+    else:
+        isSmiling = False
+        return isSmiling
+
+def drawContours(shape, frame):
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+    (mStart, mEnd) = (48, 68)
+
+    shape = face_utils.shape_to_np(shape)
+
+    mouth = shape[mStart:mEnd]
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
+    leftEAR = eyeAspectRatio(leftEye)
+    rightEAR = eyeAspectRatio(rightEye)
+
+    ear = (leftEAR + rightEAR) / 2.0
+
+    isSmiling = smileDetector(shape, ear)
+
+    leftEyeHull = cv2.convexHull(leftEye)
+    rightEyeHull = cv2.convexHull(rightEye)
+    mouthHull = cv2.convexHull(mouth)
+
+    cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+    cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+    cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
+
+    cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.putText(frame, "SMILE: {:.2f}".format(isSmiling), (300, 100),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+#
+#     # if smileGoalReached == False:
+#     #     cv2.putText(frame, "SMILETIMER: {:.2f}".format(smileDuration), (50, 30),
+#     #                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+#     # else:
+#     #     cv2.putText(frame, "Kaffee unterwegs :)", (50, 30),
+#     #                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
